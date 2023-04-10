634513915f1787406d13ad860a36781f3a881458
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-10 15:33:45 -0300

    ✨ feat: working with environment variables

--- nodejs/02-api-rest-nodejs/src/database.ts
+++ nodejs/02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,16 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+const { DATABASE_URL } = process.env
+
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- nodejs/02-api-rest-nodejs/src/server.ts
+++ nodejs/02-api-rest-nodejs/src/server.ts
@@ -1,5 +1,4 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
