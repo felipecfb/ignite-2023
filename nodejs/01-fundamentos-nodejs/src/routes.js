b3ac8a00f096437b890a54ab2188b5386720b43b
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-10 10:14:20 -0300

    ✨ feat: creating regex of params routes

--- nodejs/01-fundamentos-nodejs/src/routes.js
+++ nodejs/01-fundamentos-nodejs/src/routes.js
@@ -1,13 +1,14 @@
 import { randomUUID } from 'node:crypto';
 
 import { Database } from "./database.js";
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -17,7 +18,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -32,6 +33,13 @@ export const routes = [
       return res
         .writeHead(201)
         .end();
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      
     }
   }
 ]
