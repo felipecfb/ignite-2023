5b7e3d3bbba6febb6e34d4c73d342a582b95d9a0
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-11 11:16:54 -0300

    🚀 chore: deploy config

--- nodejs/02-api-rest-nodejs/src/database.ts
+++ nodejs/02-api-rest-nodejs/src/database.ts
@@ -2,13 +2,16 @@ import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
-const { DATABASE_URL } = env
+const { DATABASE_URL, DATABASE_CLIENT } = env
 
 export const config: Knex.Config = {
   client: 'sqlite',
-  connection: {
-    filename: DATABASE_URL,
-  },
+  connection:
+    DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: DATABASE_URL,
+        }
+      : DATABASE_CLIENT,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
