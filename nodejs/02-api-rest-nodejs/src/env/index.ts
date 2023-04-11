aa85dffbe741ee5979dc5cc7d9aa23857b717dec
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-11 10:23:58 -0300

    ✅ test: finishing tests for transactions routes

--- nodejs/02-api-rest-nodejs/src/env/index.ts
+++ nodejs/02-api-rest-nodejs/src/env/index.ts
@@ -1,5 +1,16 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+const { NODE_ENV } = process.env
+
+if (NODE_ENV === 'test') {
+  config({
+    path: '.env.test',
+    override: true,
+  })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
