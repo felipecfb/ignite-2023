25c87d76ea471b447253e7f3febfc28d33d1616e
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-11 09:24:59 -0300

    ✅ test: create a new test for new transaction

--- nodejs/02-api-rest-nodejs/src/server.ts
+++ nodejs/02-api-rest-nodejs/src/server.ts
@@ -1,26 +1,7 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
 
 const { PORT } = env
-
-const app = fastify()
-
-app.register(cookie)
-
-app.addHook('preHandler', async (request, reply) => {
-  console.log(`[${request.method}] ${request.url}`)
-})
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-app.get('/hello', () => {
-  return 'Hello world'
-})
 
 app
   .listen({
