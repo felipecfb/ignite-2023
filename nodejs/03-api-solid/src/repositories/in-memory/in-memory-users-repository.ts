07f4c63ffc0e7ff5a5c54edf33f1249088b2b6f7
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-14 17:18:22 -0300

    ✨ feat: creating use case for check in

--- nodejs/03-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
+++ nodejs/03-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { User, Prisma } from '@prisma/client'
 import { IUsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements IUsersRepository {
   public users: User[] = []
@@ -26,7 +27,7 @@ export class InMemoryUsersRepository implements IUsersRepository {
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
