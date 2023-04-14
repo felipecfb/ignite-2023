2125d9662deea5faee2a49b345be0e1b93fc720f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-14 11:07:28 -0300

    ✨ feat: implementing use case errors

--- nodejs/03-api-solid/src/use-cases/register.ts
+++ nodejs/03-api-solid/src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { IUsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface IRegisterUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findUserByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     this.usersRepository.create({
