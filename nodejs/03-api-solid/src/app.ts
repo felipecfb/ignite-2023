5a3ed2652ff78dc07b15e353cf8e2e851bfb6adb
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-27 08:47:36 -0300

    ✨ feat: implementing jwt on fastify

--- nodejs/03-api-solid/src/app.ts
+++ nodejs/03-api-solid/src/app.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 
 export const app = fastify()
+
+app.register(jwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
