9bae154ae62347126b33d1ca582e0bd13f8d718f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-27 17:10:56 -0300

    feat: implementing refresh token

--- nodejs/03-api-solid/src/app.ts
+++ nodejs/03-api-solid/src/app.ts
@@ -1,16 +1,28 @@
 import fastify from 'fastify'
-import jwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
 import { ZodError } from 'zod'
+
 import { env } from './env'
+
 import { userRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
-app.register(jwt, {
+app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(userRoutes)
 app.register(gymsRoutes)
