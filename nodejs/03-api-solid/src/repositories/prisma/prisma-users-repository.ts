32ac71b8ae009c957d292a74f8285db183634676
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-27 09:28:26 -0300

    ✨ feat: creating perfil controller

--- nodejs/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ nodejs/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -4,7 +4,13 @@ import { IUsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements IUsersRepository {
   async findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
 
   async findUserByEmail(email: string): Promise<User | null> {
