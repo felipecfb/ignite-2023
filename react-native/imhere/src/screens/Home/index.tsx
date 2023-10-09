b33c1c64dd4be03ca7d79c4cf79fe811066ba4ae
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-09 15:38:40 -0300

    feat: creating component Participant

--- react-native/imhere/src/screens/Home/index.tsx
+++ react-native/imhere/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
   function handleParticipantAdd() {
@@ -29,6 +30,10 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   )
 }
