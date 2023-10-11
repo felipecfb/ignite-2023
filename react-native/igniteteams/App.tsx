79e1cc2ad624496372db86ba1b83d329c8cbc906
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-11 16:38:57 -0300

    feat: using theme from styled components

--- react-native/igniteteams/App.tsx
+++ react-native/igniteteams/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 
 import { Groups } from "@screens/Groups";
-import theme from "src/theme";
+import theme from "./src/theme";
 
 
 export default function App() {
