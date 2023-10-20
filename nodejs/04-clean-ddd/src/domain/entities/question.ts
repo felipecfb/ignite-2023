f4abc3bdab2b0420e051a5000e9f4c8b02fe65ea
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-20 17:17:10 -0300

    feat: adding and configuring eslint

--- nodejs/04-clean-ddd/src/domain/entities/question.ts
+++ nodejs/04-clean-ddd/src/domain/entities/question.ts
@@ -1,9 +1,9 @@
-import dayjs from "dayjs"
+import dayjs from 'dayjs'
 
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { Slug } from "./value-objects/slug"
-import { Optional } from "@/core/types/optional"
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Slug } from './value-objects/slug'
+import { Optional } from '@/core/types/optional'
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -49,10 +49,7 @@ export class Question extends Entity<QuestionProps> {
   }
 
   get excerpt() {
-    return this.content
-      .substring(0, 120)
-      .trimEnd()
-      .concat('...')
+    return this.content.substring(0, 120).trimEnd().concat('...')
   }
 
   private touch() {
@@ -76,14 +73,17 @@ export class Question extends Entity<QuestionProps> {
   }
 
   static create(
-      props: Optional<QuestionProps, 'createdAt' | 'slug'>,
-      id?: UniqueEntityID
-    ) {
-    const question = new Question({
-      ...props,
-      slug: props.slug ?? Slug.createFromText(props.title),
-      createdAt: new Date()
-    }, id)
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id,
+    )
 
     return question
   }
