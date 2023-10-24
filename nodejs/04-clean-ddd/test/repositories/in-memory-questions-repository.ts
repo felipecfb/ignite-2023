e0c8f9a59907aed64876b75197c12d5e9e13cbd9
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-24 13:46:21 -0300

    feat: create use case for delete an question

--- nodejs/04-clean-ddd/test/repositories/in-memory-questions-repository.ts
+++ nodejs/04-clean-ddd/test/repositories/in-memory-questions-repository.ts
@@ -2,6 +2,18 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug)
 
@@ -12,9 +24,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
-  public items: Question[] = []
-
   async create(question: Question): Promise<void> {
     this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
