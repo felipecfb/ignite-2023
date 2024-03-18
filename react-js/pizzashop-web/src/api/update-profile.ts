76b510fe8baa60a9648cbd06bede18539ea0083b
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-03-18 14:18:11 -0300

    feat: creating a mock for profile, managed restaurant and update profile

--- react-js/pizzashop-web/src/api/update-profile.ts
+++ react-js/pizzashop-web/src/api/update-profile.ts
@@ -1,12 +1,10 @@
 import { api } from '@/lib/axios'
 
-interface UpdateProfileBody {
+export interface UpdateProfileBody {
   name: string
   description: string
 }
 
 export async function updateProfile({ name, description }: UpdateProfileBody) {
-  throw new Error('')
-
   await api.put('/profile', { name, description })
 }
